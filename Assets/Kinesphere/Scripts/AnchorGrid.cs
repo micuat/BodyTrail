4e9756b876005a09acf35365ea4b1196950b506b
micuat <micuat@gmail.com>
2019-11-04 11:41:05 +0100

    reactor

--- Assets/Kinesphere/Scripts/AnchorGrid.cs
+++ Assets/Kinesphere/Scripts/AnchorGrid.cs
@@ -10,13 +10,15 @@ public class AnchorGrid : BodyBase
 
   List<GameObject> Anchors = new List<GameObject>();
 
+  public int Size = 4;
+
   // Start is called before the first frame update
   void Start()
   {
     base.loadPlayers();
 
-    int N = 4;
-    int M = 2;
+    int N = Size;
+    int M = Size / 2;
     for (int i = -M; i <= M; i++)
     {
       for (int j = 0; j <= N; j++)
@@ -32,48 +34,17 @@ public class AnchorGrid : BodyBase
     }
   }
 
+  public List<GameObject> GetParts()
+  {
+    return parts;
+  }
+
   // Update is called once per frame
   void Update()
   {
     foreach (var anchor in Anchors)
     {
-      int count = 0;
-      int[] indices = { 1, 2, 4, 5 };
-      foreach (var index in indices)
-      {
-        GameObject part = parts[index];
-        Vector3 partPos = part.transform.position;
-        Vector3 gridPos = anchor.transform.position;
-        GridReactor reactor = anchor.GetComponent<GridReactor>();
 
-        float ix = Mathf.Floor(partPos.x * GridRes + 0.5f);
-        float iy = Mathf.Floor(partPos.y * GridRes + 0.5f);
-        float iz = Mathf.Floor(partPos.z * GridRes + 0.5f);
-
-        float s = Vector3.Distance(partPos, gridPos) * 0.5f;
-        if ((ix - reactor.i) >= 0 && (ix - reactor.i) <= 1 &&
-        (iy - reactor.j) >= 0 && (iy - reactor.j) <= 1 &&
-        (iz - reactor.k) >= 0 && (iz - reactor.k) <= 1)
-        {
-          reactor.lengths[count] = Mathf.Lerp(reactor.lengths[count], s, 0.2f);
-          anchor.transform.GetChild(count).localScale = new Vector3(0.01f, reactor.lengths[count], 0.01f);
-        }
-        else
-        {
-          reactor.lengths[count] = Mathf.Lerp(reactor.lengths[count], 0, 0.2f);
-          if (reactor.lengths[count] < 0.01f)
-          {
-            anchor.transform.GetChild(count).localScale = new Vector3(0, 0, 0);
-          }
-          else
-          {
-            anchor.transform.GetChild(count).localScale = new Vector3(0.01f, reactor.lengths[count], 0.01f);
-          }
-        }
-        anchor.transform.GetChild(count).position = Vector3.Lerp(partPos, gridPos, 0.5f);
-        anchor.transform.GetChild(count).rotation = Quaternion.FromToRotation(Vector3.up, partPos - gridPos);
-        count++;
-      }
     }
   }
 }
--- Assets/Kinesphere/Scripts/GridReactor.cs
+++ Assets/Kinesphere/Scripts/GridReactor.cs
@@ -33,5 +33,42 @@ public class GridReactor : MonoBehaviour
   // Update is called once per frame
   void Update()
   {
+    float GridRes = transform.parent.GetComponent<AnchorGrid>().GridRes;
+    int count = 0;
+    int[] indices = { 1, 2, 4, 5 };
+    foreach (var index in indices)
+    {
+      GameObject part = transform.parent.GetComponent<AnchorGrid>().GetParts()[index];
+      Vector3 partPos = part.transform.position;
+      Vector3 gridPos = transform.position;
+
+      float ix = Mathf.Floor(partPos.x * GridRes + 0.5f);
+      float iy = Mathf.Floor(partPos.y * GridRes + 0.5f);
+      float iz = Mathf.Floor(partPos.z * GridRes + 0.5f);
+
+      float s = Vector3.Distance(partPos, gridPos) * 0.5f;
+      if ((ix - i) >= 0 && (ix - i) <= 1 &&
+      (iy - j) >= 0 && (iy - j) <= 1 &&
+      (iz - k) >= 0 && (iz - k) <= 1)
+      {
+        lengths[count] = Mathf.Lerp(lengths[count], s, 0.2f);
+        transform.GetChild(count).localScale = new Vector3(0.01f, lengths[count], 0.01f);
+      }
+      else
+      {
+        lengths[count] = Mathf.Lerp(lengths[count], 0, 0.2f);
+        if (lengths[count] < 0.01f)
+        {
+          transform.GetChild(count).localScale = new Vector3(0, 0, 0);
+        }
+        else
+        {
+          transform.GetChild(count).localScale = new Vector3(0.01f, lengths[count], 0.01f);
+        }
+      }
+      transform.GetChild(count).position = Vector3.Lerp(partPos, gridPos, 0.5f);
+      transform.GetChild(count).rotation = Quaternion.FromToRotation(Vector3.up, partPos - gridPos);
+      count++;
+    }
   }
 }
